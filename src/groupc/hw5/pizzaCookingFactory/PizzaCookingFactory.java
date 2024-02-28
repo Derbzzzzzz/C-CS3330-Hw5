abfb75a4010f3d0bfbb9a11376cc371a460b5620
Ryan Wahle <ryanwahle8@gmail.com>
2024-02-27 19:58:44 -0600

    Added method for PizzaCookingFactory

--- src/groupc/hw5/pizzaCookingFactory/PizzaCookingFactory.java
+++ src/groupc/hw5/pizzaCookingFactory/PizzaCookingFactory.java
@@ -1,5 +1,23 @@
 package groupc.hw5.pizzaCookingFactory;
 
+import groupc.hw5.pizza.AbstractPizza;
+import groupc.hw5.pizza.PizzaType;
+
 public class PizzaCookingFactory {
+	
+	/**
+	 *  This method creates an AbstractPizza instance and instantiates 
+	 *  it according to the pizzaType parameter. It also sets 
+	 *  the pizzaOrderID of the pizza using the current orderIDCounter 
+	 *  of the pizza. The orderIDCounter will be incremented and 
+	 *  assigned automatically on every creation of pizza, 
+	 *  to assign unique pizza order IDs.
+	 * 
+	 * @param pizzaType
+	 * @return AbstractPizza
+	 */
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		return null;
+	}
 
 }
